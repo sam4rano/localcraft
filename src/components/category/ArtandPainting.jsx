930c74040f152b73d6e1e9eb5d247cc5fe85a38e
Samuel Oyerinde <oyerindesamuelabiodun@gmail.com>
2024-05-01 13:42:51 +0100

    shop started

--- src/components/category/ArtandPainting.jsx
+++ src/components/category/ArtandPainting.jsx
@@ -1,9 +1,76 @@
+// Mock data for the products
+import paint_one from "../../assets/painting_one.png";
+import paint_two from "../../assets/painting_two.png";
+import art_one from "../../assets/art_one.png";
 
+const products = [
+  {
+    id: 1,
+    image: paint_one,
+    title: "Black painting",
+    price: "$200",
+  },
+  {
+    id: 2,
+    image: paint_two,
+    title: "Beauty painting",
+    price: "$200",
+  },
+
+  {
+    id: 3,
+    image: paint_two,
+    title: "Dior painting",
+    price: "$200",
+  },
+  {
+    id: 4,
+    image: paint_one,
+    title: "EastSide painting",
+    price: "$200",
+  },
+  {
+    id: 5,
+    image: paint_two,
+    title: "Clotha painting",
+    price: "$200",
+  },
+
+  {
+    id: 6,
+    image: paint_two,
+    title: "Black beauty painting",
+    price: "$200",
+  },
+];
 
 const ArtandPainting = () => {
   return (
-	<div>ArtandPainting</div>
-  )
-}
+    <div className="flex align-middle flex-col py-[20px] my-[20px] px-[10px] gap-[20px]">
+      <h2 className="text-[40px] leading-[50px] font-medium text-center">Art And Painting</h2>
 
-export default ArtandPainting
+      <div className="w-[100%] flex flex-row gap-[20px]">
+        {products.map((product) => (
+          <ul key={product.id} className="w-full gap-[10px] flex flex-wrap">
+            <div className="bg-primary rounded-lg shadow-md overflow-hidden p-[12px] ">
+              <img src={product.image} alt={product.title} className="w-full" />
+              <div className="p-[10px] flex w-full flex-col">
+                <h3 className="text-lg font-semibold text-black">
+                  {product.title}
+                </h3>
+                <div className="flex flex-row justify-between align-middle items-center">
+                  <p className="text-gray-500">{product.price}</p>
+                  <button className="p-[5px] text-white bg-black rounded hover:bg-gray-800 focus:outline-none focus:bg-gray-800">
+                    Add to cart
+                  </button>
+                </div>
+              </div>
+            </div>
+          </ul>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ArtandPainting;
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import ArtandPainting from '../components/category/ArtandPainting';
+import Glassblowing from '../components/category/GlassBlowing';
+import MetalWork from '../components/category/MetalWork';
+import Pottery from '../components/category/Pottery';
+import WoodWork from '../components/category/WoodWork';
 
 const Shop = () => {
   return (
-	<div>Shop</div>
+	<div>
+    <div>
+      <h2>Mind Blowing Craft for a mind blowing home decoration</h2>
+      <h5>Give your apartment a facelift</h5>
+    </div>
+    <div className='flex flex-col align-middle'>
+      <ArtandPainting />
+      <Glassblowing />
+      <MetalWork />
+      <Pottery />
+      <WoodWork />
+    </div>
+  </div>
   )
 }
 
